661442b12ec4ba454557cca92021b9c683cbeb17
YohannaToro <yoha_duran156@hotmail.com>
2019-11-04 01:30:17 -0500

    almost done

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -1,137 +1,158 @@
-import React,{Component} from 'react'
-import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBBtn, MDBModalFooter } from 'mdbreact';
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
-import TodoList from './todoList'
-import TaskForm from './todoForm'
-import {Link} from 'react-router-dom'
-import '../css/todo.scss'
-import FilterForm from './complements/cardList'
-import FilterResults from 'react-filter-search';
+import React, { Component } from "react";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBBtn,
+  MDBModalFooter
+} from "mdbreact";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import TodoList from "./todoList";
+import TaskForm from "./todoForm";
+import { Link } from "react-router-dom";
+import "../css/todo.scss";
+import FilterForm from "./complements/cardList";
+import FilterResults from "react-filter-search";
+import axios from "axios";
 
-export default class TodoBox extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            show:false,fil:false,value:"",
-            listfilter:[],fil:false,update:false,prueba:[],
-           data:[ {
-               "title":"lab4",
-            "description": "some description text ",
-            "responsible": {
-                "name": "Santiago Carrillo",
-                "email": "sancarbar@gmail"
-            },
-            "status": "ready",
-            "dueDate": 156464645646
-        }],
-            userInput:''
+export default class TodoBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      fil: false,
+      value: "",
+      listfilter: [],
+      fil: false,
+      update: false,
+      prueba: [],
+      data: [
+        {
+          title: "lab4",
+          description: "some description text ",
+          responsible: {
+            name: "Santiago Carrillo",
+            email: "sancarbar@gmail"
+          },
+          status: "ready",
+          dueDate: 156464645646
         }
-    }
-    componentDidMount() {
-      console.log("holaaaaaaaaaaaaaaaaaaaaaaaa")
-      fetch('http://localhost:8080/boards/1')
+      ],
+      userInput: ""
+    };
+  }
+  componentDidMount() {
+    console.log("holaaaaaaaaaaaaaaaaaaaaaaaa");
+    fetch("http://localhost:8080/boards/1")
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => console.log(json));
+  }
+
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState({ value });
+  };
+  changeUSerInput(input) {
+    this.setState({ userInput: input });
+  }
+  addTask(input) {
+    let array = this.state.list;
+    array.push(input);
+    this.setState({ list: array, userInput: "" });
+  }
+  handleCommentSubmit(task) {
+    let tasks = this.state.data;
+    var newTasks = tasks.concat([task]);
+    this.setState({ data: newTasks });
+    axios
+      .post("localhost:8080/boards/tasks/addTask", task, {
+        // receive two parameter endpoint url ,form data
+      })
+      .then(res => {
+        // then print response status
+        console.log(res.statusText);
+      });
+  }
+  handleFilterSubmit(filter) {
+    console.log("updatedList");
+    console.log(filter);
+    this.setState({ update: true });
+    this.setState({ list: filter });
+  }
+  HandlerUpdateList(props) {
+    const isUpdate = props.isUpdate;
+    if (isUpdate) {
+      return <TodoList data={this.state.list} />;
     }
+    this.setState({ list: [] });
+    return <TodoList data={this.state.data} />;
+  }
 
+  render() {
+    const handleClose = () => this.setState({ show: false });
+    const handleShow = () => this.setState({ show: true });
+    const handleFil = () => this.setState({ fil: true });
+    const handleFilClose = () => this.setState({ fil: false });
+    const handleUpdate = () => this.setState({ update: true });
+    const isLoggedIn = this.state.update;
+    return (
+      <div>
+        {isLoggedIn ? (
+          <TodoList data={this.state.list} onClick={handleUpdate} />
+        ) : (
+          <TodoList data={this.state.data} />
+        )}
 
-    
-    handleChange = event => {
-        const { value } = event.target;
-        this.setState({ value });
-      };
-    changeUSerInput(input){
-        this.setState({userInput:input});
+        <Button
+          className="bt"
+          style={{ backgroundColor: "black", color: "white" }}
+          onClick={handleShow}
+        >
+          New Task
+        </Button>
+        <Button
+          className="bt"
+          style={{ backgroundColor: "black", color: "white" }}
+          onClick={handleFil}
+        >
+          Filter task
+        </Button>
 
-    }
-    addTask(input){
-        let array=this.state.list;
-        array.push(input);
-        this.setState({list:array,userInput:""
-        })
-    }
-    handleCommentSubmit(task) {
-        let tasks = this.state.data;
-        var newTasks = tasks.concat([task]);
-        this.setState({data: newTasks});    
-     
-    } 
-    handleFilterSubmit(filter) {
-        console.log("updatedList")
-        console.log(filter)
-        this.setState({update:true})
-        this.setState({list:filter})
-        
-      }
-      HandlerUpdateList(props){
-        const isUpdate = props.isUpdate;
-        if (isUpdate) {
-          return <TodoList data={this.state.list}/>
-        }
-        this.setState({list:[]})
-        return <TodoList data={this.state.data}/>;
-
-      }
-        
-    render(){
-      
-        const handleClose = () => this.setState({show:false});
-        const handleShow = () => this.setState({show:true});
-        const handleFil=()=>this.setState({fil:true});
-        const handleFilClose=()=>this.setState({fil:false});
-        const handleUpdate=()=>this.setState({update:true});
-        const isLoggedIn = this.state.update;
-        return(
-            <div >
-        
-           {isLoggedIn ? (
-            
-        <TodoList data={this.state.list} onClick={handleUpdate}/>
-      ) : (
-        <TodoList data={this.state.data}/>
-      )}
-       
-               <Button className="bt" style={{backgroundColor:'black',color:'white'}} onClick={handleShow}>
-     New Task
-      </Button>
-      <Button className="bt" style={{backgroundColor:'black',color:'white'}} onClick={handleFil}>
-     Filter task
-      </Button>
-           
-      <Modal show={this.state.show} onHide={handleClose} className="center">
-        <Modal.Header closeButton className="back">
-          <Modal.Title>ADD NEW TASK</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="back">
-        <TaskForm onTasktSubmit={task => this.handleCommentSubmit(task)} />
-
-        </Modal.Body>
-      
-      </Modal>
-      <Modal show={this.state.fil} onHide={handleFilClose} className="center">
-        <Modal.Header closeButton className="back">
-          <Modal.Title>Filter task</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="back">
-            
-        <FilterForm data={this.state.data} onFiltertSubmit={filter => this.handleFilterSubmit(filter)} />
-        </Modal.Body>
-        <Modal.Footer className="back">
-          <Button  style={{background:"black",color:"white"}} onClick={handleFilClose}>
-            Close
-          </Button>
-          <Button color="black" variant="primary" onClick={handleFilClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    
-               
-          
-            </div>
-          
-
-        )
-    }
+        <Modal show={this.state.show} onHide={handleClose} className="center">
+          <Modal.Header closeButton className="back">
+            <Modal.Title>ADD NEW TASK</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="back">
+            <TaskForm onTasktSubmit={task => this.handleCommentSubmit(task)} />
+          </Modal.Body>
+        </Modal>
+        <Modal show={this.state.fil} onHide={handleFilClose} className="center">
+          <Modal.Header closeButton className="back">
+            <Modal.Title>Filter task</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="back">
+            <FilterForm
+              data={this.state.data}
+              onFiltertSubmit={filter => this.handleFilterSubmit(filter)}
+            />
+          </Modal.Body>
+          <Modal.Footer className="back">
+            <Button
+              style={{ background: "black", color: "white" }}
+              onClick={handleFilClose}
+            >
+              Close
+            </Button>
+            <Button color="black" variant="primary" onClick={handleFilClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
 }
--- src/components/todoForm.js
+++ src/components/todoForm.js
@@ -1,147 +1,182 @@
-import React, { Component } from 'react'
-import Input from '@material-ui/core/Input';
-import Button from '@material-ui/core/Button';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon, MDBInput } from 'mdbreact';
-
+import React, { Component } from "react";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
+import axios from 'axios';
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBBtn,
+  MDBIcon,
+  MDBInput
+} from "mdbreact";
+import ApiService from "./services/apiServices";
 import DatePicker from "react-datepicker";
- 
+
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class taskForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            status:"",
-            description:'',
-            title:'',
-            name:"",
-            responsible: 
-            {
-                email: "",userName: ""
-            },
-            dueDate:new Date()
-        }
-   
-       }
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: "",
+      description: "",
+      title: "",
+      name: "",
+      responsible: {
+        email: "",
+        userName: ""
+      },
+      dueDate: new Date(),
+      selectedFile: null,fileName:""
+    };
+  }
 
-       handleDescriptionChange(e) {
-        this.setState({description: e.target.value});
+  handleDescriptionChange(e) {
+    this.setState({ description: e.target.value });
+  }
+  handleNameChange(e) {
+    this.setState({ name: e.target.value });
+  }
+  handleResponsibleEmailChange(e) {
+    this.setState({ responsible: { email: e.target.value } });
+  }
+
+  onChangeHandler = event => {
+    console.log(event.target.files[0]);
+    this.setState({
+      selectedFile: event.target.files[0],fileName:event.target.files[0].name
+    });
+  };
+
+  handleTextChange(e) {
+    this.setState({ title: e.target.value });
+  }
+  handlePirorityChange(e) {
+    this.setState({ status: e.target.value });
+  }
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+  handleSubmit(e) {
+    e.preventDefault();
+
+    var text = this.state.title.trim();
+    var prior = this.state.status.trim();
+    var des = this.state.description.trim();
+    var resName = this.state.name.trim();
+    var resEmail = this.state.responsible.email.trim();
+    var d = this.state.dueDate;
+    var fileNam=this.state.fileName;
+    console.log(d);
+
+    if (!text || !prior) {
+      return;
     }
-    handleNameChange(e){
-      
-        this.setState({name:e.target.value})
-       
-    }
-    handleResponsibleEmailChange(e){
-        this.setState({responsible:{email:e.target.value}})
-    }
+    const data = new FormData() 
+    data.append('file', this.state.selectedFile)
+    axios.post("http://localhost:8080/boards/upload", data, { // receive two parameter endpoint url ,form data 
+      })
+      .then(res => { // then print response status
+        console.log(res.statusText)
+      })
+    this.props.onTasktSubmit({
+      title: text,
+      startDate: prior,
+      dueDate: d,
+      description: des,
+      responsible: {
+        name: resName,
+        email: resEmail
+      },fileName:fileNam
+    });
+    this.setState({
+      name: "",
+      status: "",
+      description: "",
+      title: "",
+      responsible: {
+        some: "",
+        email: ""
+      },
+      dueDate: new Date(),fileName:""
+    });
+  }
 
+  render() {
+    return (
+      <MDBRow className="form-modal ">
+        <MDBCol md="6" className="modal-style">
+          <form className="task-Formi" onSubmit={e => this.handleSubmit(e)}>
+            <MDBInput
+              className="input-content"
+              type="text"
+              label="title"
+              placeholder="title"
+              icon="pencil-alt"
+              value={this.state.title}
+              onChange={e => this.handleTextChange(e)}
+            />
+            <MDBInput
+              className="input-pririty"
+              type="text"
+              label="Status"
+              icon="bookmark"
+              placeholder="status"
+              value={this.state.status}
+              onChange={e => this.handlePirorityChange(e)}
+            />
+            <MDBInput
+              className="input-pririty"
+              type="text"
+              label="Status"
+              icon="bookmark"
+              placeholder="Description"
+              value={this.state.description}
+              onChange={e => this.handleDescriptionChange(e)}
+            />
+            <MDBInput
+              className="input-pririty"
+              type="text"
+              label="Responsible user"
+              icon="bookmark"
+              placeholder="Responsible name"
+              value={this.state.name}
+              onChange={e => this.handleNameChange(e)}
+            />
+            <MDBInput
+              className="input-pririty"
+              type="text"
+              label="Responsible email"
+              icon="bookmark"
+              placeholder="Add a description"
+              value={this.state.responsible.email}
+              onChange={e => this.handleResponsibleEmailChange(e)}
+            />
 
-   
-        handleTextChange(e) {
-            this.setState({title: e.target.value});
-        }
-        handlePirorityChange(e){
-            this.setState({status:e.target.value})
-        }
-        handleChange = date => {
-            this.setState({
-              startDate: date
-            });
-          };
-        handleSubmit(e) {
-            e.preventDefault();
-           
-            var text= this.state.title.trim();
-            var prior=this.state.status.trim();
-            var des=this.state.description.trim();
-            var resName=this.state.name.trim();
-            var resEmail=this.state.responsible.email.trim();
-            var d=this.state.dueDate;
-            console.log(d);
-            if (!text || !prior) {
-                return;
-            }
-            this.props.onTasktSubmit({ title: text,startDate:prior,dueDate:d,description:des,responsible: {
-                "name": resName,
-                "email": resEmail
-            }});
-            this.setState({
-                name:"",
-                status:"",
-                description:'',
-                title:'',
-                responsible: {
-                    some: "",
-                    email: ""
-                },
-                dueDate:new Date()
-            });
-        }
-        
-    render() {
-        return (
-            <MDBRow className="form-modal " >
-            <MDBCol md="6" className="modal-style">
-            <form className="task-Formi" onSubmit={(e) => this.handleSubmit(e)}> 
-		
-			 <MDBInput
-			 	className="input-content"
-                 type="text"
-                 label="title"
-                 placeholder="title"
-                 icon="pencil-alt"
-				 value={this.state.title}
-				 onChange={(e) => this.handleTextChange(e)} 
-			 /> 
-             <MDBInput
-			 	className="input-pririty"
-                 type="text"
-                 label="Status"
-                 icon="bookmark"
-				 placeholder="status"
-				 value={this.state.status}
-				 onChange={(e) => this.handlePirorityChange(e)} 
-			 /> 
-             <MDBInput
-			 	className="input-pririty"
-                 type="text"
-                 label="Status"
-                 icon="bookmark"
-				 placeholder="Description"
-				 value={this.state.description}
-				 onChange={(e) => this.handleDescriptionChange(e)} 
-			 /> 
-             <MDBInput
-			 	className="input-pririty"
-                 type="text"
-                 label="Responsible user"
-                 icon="bookmark"
-                 placeholder="Responsible name"
-				 value={this.state.name}
-				 onChange={(e) => this.handleNameChange(e)} 
-			 />
-             <MDBInput
-			 	className="input-pririty"
-                 type="text"
-                 label="Responsible email"
-                 icon="bookmark"
-				 placeholder="Add a description"
-				 value={this.state.responsible.email}
-				 onChange={(e) => this.handleResponsibleEmailChange(e)} 
-			 />
+            <input type="file" name="file" onChange={this.onChangeHandler} />
 
-         <DatePicker
-         icon="calendar-plus" 
-        selected={this.state.dueDate}
-        onChange={this.handleChange}
-      />
+            <DatePicker
+              icon="calendar-plus"
+              selected={this.state.dueDate}
+              onChange={this.handleChange}
+            />
 
-
-			<p><Button type="submit" className="btn-right" variant="outlined" color="primary">
-                <a >Add Task</a></Button></p>
-			 
-		 </form></MDBCol></MDBRow>
-        )
-    }
+            <p>
+              <Button
+                type="submit"
+                className="btn-right"
+                variant="outlined"
+                color="primary"
+              >
+                <a>Add Task</a>
+              </Button>
+            </p>
+          </form>
+        </MDBCol>
+      </MDBRow>
+    );
+  }
 }
